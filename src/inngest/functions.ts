c310005d2473df369c723023284d6c2f9d2bd21b
Jacob Heric <jacob@jacobheric.com>
2024-10-02 17:02:42 -0400

    add some logging

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,6 +4,7 @@ export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    console.log("running hello world");
     await step.sleep("wait-a-moment", "1s");
     return { event, body: "Hello, World!" };
   }
