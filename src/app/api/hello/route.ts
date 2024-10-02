09d6e4997ffcec87c7c001ec091f31c3c1823b34
Jacob Heric <jacob@jacobheric.com>
2024-10-02 17:14:38 -0400

    wire up hello route correctly

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,8 +1,18 @@
-import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { helloWorld } from "@/inngest/functions";
+import { NextResponse } from "next/server";
+import { inngest } from "../../../inngest/client"; // Import our client
 
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [helloWorld],
-});
+// Opt out of caching; every request should send a new event
+export const dynamic = "force-dynamic";
+
+// Create a simple async Next.js API route handler
+export async function GET() {
+  // Send your event payload to Inngest
+  await inngest.send({
+    name: "test/hello.world",
+    data: {
+      email: "test@example.com",
+    },
+  });
+
+  return NextResponse.json({ name: "Hello Inngest from Next!" });
+}
